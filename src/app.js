c7d24fbfbce9c1441e05ed18016bca6aaa2de991
Erik Malo Schuessler <90277878+Brainspeck@users.noreply.github.com>
2025-10-29 02:43:25 +0000

    started the project and added basic JS functionality

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let excuseElement = this.document.querySelector("#excuse")
+  
+  excuseElement.innerHTML = `${generateExcuse(who)} ${generateExcuse(action)} ${generateExcuse(what)} ${generateExcuse(when)}`;
 };
+
+
+function generateExcuse(list) {
+  let randomExcuse = Math.floor(Math.random() * list.length);
+  return list[randomExcuse];
+}
